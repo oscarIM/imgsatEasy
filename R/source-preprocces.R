7a6b75dc5000d8db2db7b4fc27a12ef97cd1283e
oscarIM <oskinostroza@gmail.com>
2022-06-10 13:32:36 -0400

    test sh

--- R/source-preprocces.R
+++ R/source-preprocces.R
@@ -83,6 +83,7 @@ get_L3 <- function(dir_input, dir_output, var_name, n_cores = 1, res_l2 = "1", r
     l2bin <- system.file("inst", "l2bin.sh", package = "imgsatEasy")
     l3bin <- system.file("inst", "l3bin.sh", package = "imgsatEasy")
     l3mapgen <- system.file("inst", "l3mapgen.sh", package = "imgsatEasy")
+    system2(command = "chmod +x", args =l2bin)
     if (var_name == "sst"){
       flaguse <- "LAND, HISOLZEN"
     } else {
