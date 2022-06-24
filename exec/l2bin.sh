eb2c55771f478a1f8fb40d66f39270fc7280aa7b
oscarIM <oskinostroza@gmail.com>
2022-06-24 15:12:19 -0400

    add extra stoutput

--- exec/l2bin.sh
+++ exec/l2bin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export OCSSWROOT=${OCSSWROOT:-/home/evolecolab/seadas/ocssw}
 source ${OCSSWROOT}/OCSSW_bash.env
-l2bin infile=${1} ofile=${2} prodtype=${3} l3bprod=${4} resolve=${5} verbose=${6} flaguse=${7} qual_max=${8}
+l2bin infile=${1} ofile=${2} prodtype=${3} l3bprod=${4} resolve=${5} verbose=${6} flaguse=${7} qual_max=${8} >${1}"_l2binlog.txt"
 
