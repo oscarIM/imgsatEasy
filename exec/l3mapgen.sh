24086e3f923f798c9f105c21351f06a53f4a3cd4
oscarIM <oskinostroza@gmail.com>
2022-06-28 16:28:44 -0400

    fix shells

--- exec/l3mapgen.sh
+++ exec/l3mapgen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export OCSSWROOT=${OCSSWROOT:-/home/evolecolab/seadas/ocssw}
 source ${OCSSWROOT}/OCSSW_bash.env
-  l3mapgen ifile=${1} ofile=${2} product=${3} oformat=${4} resolution=${5} projection=${6} interp=${7} north=${8} south=${9} west=${10} east=${11} quiet=${12} mask_land=${11}
+  l3mapgen ifile=${1} ofile=${2} product=${3} oformat=${4} resolution=${5} projection=${6} interp=${7} north=${8} south=${9} west=${10} east=${11} quiet=${12} mask_land=${13}
 mkdir log_files
 mv *.txt log_files
 rm *_tmp.nc
