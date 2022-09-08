9974f7d2b71adaedc8ceae4e36e627ccc36de470
oscarIM <oskinostroza@gmail.com>
2022-09-08 12:12:20 -0400

    testing l2bin options

--- R/get-L3.R
+++ R/get-L3.R
@@ -148,7 +148,7 @@ get_L3 <- function(dir_ocssw, dir_input, dir_output, var_name, n_cores = 1, res_
   seadas_l2bin <- function(infile, ofile) {
     {
       system2(command = "chmod", args = c("+x", seadas_bins[1]))
-      system2(command = seadas_bins[1], args = c(infile, ofile, "day", var_name, res_l2, "off", flaguse, "0"))
+      system2(command = seadas_bins[1], args = c(infile, ofile, "day", var_name, res_l2, "off", flaguse, "0",  north, south, east, west, 10, 1))
     } %>% possibly(., otherwise = "Error en archivo de entrada")
   }
 
