ecab31338a886dd03445f6de44268ec350ef6a02
oscarIM <oskinostroza@gmail.com>
2022-09-09 11:13:48 -0400

    testing l2bin

--- exec/l2bin.sh
+++ exec/l2bin.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export OCSSWROOT=${OCSSWROOT:-/home/evolecolab/seadas/ocssw}
 source ${OCSSWROOT}/OCSSW_bash.env
-l2bin infile=${1} ofile=${2} prodtype=${3} l3bprod=${4} resolve=${5} verbose=${6} flaguse=${7} qual_max=${8} latnorth=${9} latsouth=${10} loneast=${11} lonwest=${12} > ${1}"_l2binlog.txt"
+l2bin infile=${1} ofile=${2} prodtype=${3} l3bprod=${4} resolve=${5} verbose=${6} flaguse=${7} qual_max=${8} latnorth=${9} latsouth=${10} loneast=${11} lonwest=${12} area_weighting=${13} > ${1}"_l2binlog.txt"
