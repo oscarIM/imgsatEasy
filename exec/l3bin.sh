0bd01f349d190231175087bd2c9e1cc22bc6d5c3
oscarIM <oskinostroza@gmail.com>
2023-01-06 11:30:36 -0300

    testing new l2bin

--- exec/l3bin.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export OCSSWROOT=${OCSSWROOT:-/home/evolecolab/seadas/ocssw}
-source ${OCSSWROOT}/OCSSW_bash.env
-l3bin infile=${1} ofile=${2} prod=${3} oformat=${4} verbose=${5} > ${1}"_l3binlog.txt"
--- exec/l3mapgen.sh
+++ exec/l3mapgen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export OCSSWROOT=${OCSSWROOT:-/home/evolecolab/seadas/ocssw}
 source ${OCSSWROOT}/OCSSW_bash.env
-  l3mapgen ifile=${1} ofile=${2} product=${3} oformat=${4} resolution=${5} projection=${6} interp=${7} north=${8} south=${9} west=${10} east=${11} quiet=${12} mask_land=${13} fudge=${14}> ${1}"_l3mapgenlog.txt"
+  l3mapgen ifile=${1} ofile=${2} product=${3} oformat=${4} resolution=${5} projection=${6} interp=${7} north=${8} south=${9} west=${10} east=${11} quiet=${12} mask_land=${13} fudge=${14}> {2}"_l3mapgenlog.txt"
 
