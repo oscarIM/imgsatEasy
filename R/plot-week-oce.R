eac0a78e081d981d5c382e0499038c67e1b08e42
Default Seadas User <seadas-user@localhost>
2023-04-03 12:15:18 -0400

    typo error

--- R/plot-week-oce.R
+++ R/plot-week-oce.R
@@ -8,10 +8,10 @@
 #' @param shp_file nombre del shp utilizado para la imagen final
 #' @param start_time fecha de inicio del el plot
 #' @param end_time fecha de termino del plot
-#' @importFrom purrr map2 
+#' @importFrom purrr map2
 #' @importFrom tidyr pivot_longer
 #' @importFrom oce oce.colorsJet
-#' @importFrom metR metR::scale_x_longitude metR::scale_y_latitude
+#' @importFrom metR scale_x_longitude scale_y_latitude
 #' @importFrom patchwork plot_annotation
 #' @importFrom oce oce.colorsJet
 #' @importFrom RColorBrewer brewer.pal
@@ -31,7 +31,7 @@
 #' week_number <- 20
 #' start_time <- "2023-03-20"
 #' end_time <- "2023-03-26"
-#' name_plot <- "ocepars_plot_w20.png" 
+#' name_plot <- "ocepars_plot_w20.png"
 #' plot_week_oce(lists_nc = lists_nc, week_number = week_number,name_plot = name_plot,height = height, width = width,shp_file = shp_file,start_time  = start_time, end_time = end_time)
 #' }
 plot_week_oce <- function(lists_nc, week_number, name_plot, height, width, shp_file, start_time, end_time){
@@ -46,8 +46,8 @@ plot_week_oce <- function(lists_nc, week_number, name_plot, height, width, shp_f
   data_to_filter <- sf::st_intersects(data_sf, shp_sf)
   df_to_plot <- df_to_plot %>% dplyr::mutate("inside" = apply(data_to_filter, 1, any))
   bbox <- sf::st_bbox(shp_sf)
-  data <- df_to_plot %>% dplyr::filter(inside == "FALSE") %>% 
-    dplyr::filter(dplyr::between(x, bbox[1], bbox[3])) %>% 
+  data <- df_to_plot %>% dplyr::filter(inside == "FALSE") %>%
+    dplyr::filter(dplyr::between(x, bbox[1], bbox[3])) %>%
     dplyr::filter(dplyr::between(y, bbox[2], bbox[4]))
 xlim <- c(bbox[1], bbox[3])
 ylim <- c(bbox[2], bbox[4])
@@ -58,7 +58,7 @@ plot_chlor_a <- ggplot(data %>% filter(vars == "chlor_a")) +
                        limits = c(-1, 2),
                        oob = squish,
                        breaks = c(-1, 0, 1, 2),
-                       labels = c(expression(10^-1), expression(10^0), expression(10^1), expression(10^2))) + 
+                       labels = c(expression(10^-1), expression(10^0), expression(10^1), expression(10^2))) +
   metR::scale_x_longitude(ticks = 0.1) +
   metR::scale_y_latitude(ticks = 0.1) +
   coord_equal() +
@@ -70,11 +70,11 @@ plot_chlor_a <- ggplot(data %>% filter(vars == "chlor_a")) +
     }))),
     title.position = "right",
     title.theme = element_text(angle = 90),
-    barwidth = .5, 
-    barheight = 12, 
+    barwidth = .5,
+    barheight = 12,
     title.hjust = .5
   )) +
-  theme_bw() 
+  theme_bw()
 ###plot sst
 blues <- rev(brewer.pal(9, "YlGnBu"))
 reds <- brewer.pal(9, "YlOrRd")
@@ -86,25 +86,25 @@ plot_sst <- ggplot(data %>% filter(vars == "sst")) +
   metR::scale_y_latitude(ticks = 0.1) +
   coord_equal() +
   geom_sf(data = shp_sf, fill = "grey80", col = "black") +
-  coord_sf(xlim = xlim , ylim = ylim) + 
+  coord_sf(xlim = xlim , ylim = ylim) +
   guides(fill = guide_colorbar(
         title = "Temperatura Superficial \n Mar (°C)",
         title.position = "right",
         title.theme = element_text(angle = 90),
-        barwidth = .5, 
-        barheight = 12, 
+        barwidth = .5,
+        barheight = 12,
         title.hjust = .5
       )) +
       theme_bw()
 plot_rrs <- ggplot(data %>% filter(vars == "rrs_645")) +
-  geom_raster(aes(x, y, fill = fill)) + 
+  geom_raster(aes(x, y, fill = fill)) +
   scale_fill_gradientn(colours = oce.colorsJet(120), na.value = "white",
                        n.breaks = 5) +
   metR::scale_x_longitude(ticks = 0.1) +
   metR::scale_y_latitude(ticks = 0.1) +
   coord_equal() +
   geom_sf(data = shp_sf, fill = "grey80", col = "black") +
-  coord_sf(xlim = xlim , ylim = ylim) + 
+  coord_sf(xlim = xlim , ylim = ylim) +
   guides(fill = guide_colorbar(
         title = expression(paste(
           "nWLR 645",
